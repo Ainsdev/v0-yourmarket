f96bb1d096cc2f778fbfe33d59c06e512059be78
Ainsdev <ainsn1654@gmail.com>
2024-03-11 23:25:50 -0300

    Fix imports and add 'images' field to the 'posts' table schema

--- src/lib/db/schema/posts.ts
+++ src/lib/db/schema/posts.ts
@@ -2,16 +2,18 @@ import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
-import { stores } from "./stores"
+import { stores } from "./stores";
 import { type getPosts } from "@/lib/api/posts/queries";
 
 import { nanoid, timestamps } from "@/lib/utils";
 
-
-export const posts = sqliteTable('posts', {
-  id: text("id").primaryKey().$defaultFn(() => nanoid()),
+export const posts = sqliteTable("posts", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
   name: text("name").notNull(),
   price: integer("price").notNull(),
+  images: text("images", { mode: "json" }).notNull(),
   storeId: text("store_id").notNull(),
   createdAt: text("created_at")
     .notNull()
@@ -19,26 +21,26 @@ export const posts = sqliteTable('posts', {
   updatedAt: text("updated_at")
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
-
 });
-
 
 // Schema for posts - used to validate API requests
-const baseSchema = createSelectSchema(posts).omit(timestamps)
+const baseSchema = createSelectSchema(posts).omit(timestamps);
 
 export const insertPostSchema = createInsertSchema(posts).omit(timestamps);
-export const insertPostParams = baseSchema.extend({
-  price: z.coerce.number(),
-  storeId: z.coerce.string().min(1)
-}).omit({ 
-  id: true
-});
+export const insertPostParams = baseSchema
+  .extend({
+    price: z.coerce.number(),
+    storeId: z.coerce.string().min(1),
+  })
+  .omit({
+    id: true,
+  });
 
 export const updatePostSchema = baseSchema;
 export const updatePostParams = baseSchema.extend({
   price: z.coerce.number(),
-  storeId: z.coerce.string().min(1)
-})
+  storeId: z.coerce.string().min(1),
+});
 export const postIdSchema = baseSchema.pick({ id: true });
 
 // Types for posts - used to type API request params and within Components
@@ -47,7 +49,8 @@ export type NewPost = z.infer<typeof insertPostSchema>;
 export type NewPostParams = z.infer<typeof insertPostParams>;
 export type UpdatePostParams = z.infer<typeof updatePostParams>;
 export type PostId = z.infer<typeof postIdSchema>["id"];
-    
-// this type infers the return from getPosts() - meaning it will include any joins
-export type CompletePost = Awaited<ReturnType<typeof getPosts>>["posts"][number];
 
+// this type infers the return from getPosts() - meaning it will include any joins
+export type CompletePost = Awaited<
+  ReturnType<typeof getPosts>
+>["posts"][number];
