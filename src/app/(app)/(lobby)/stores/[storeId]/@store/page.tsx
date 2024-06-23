1f92d808049c6a33c2f8179f64fa0d1e2e37152e
Ainsdev <ainsn1654@gmail.com>
2024-06-22 21:55:36 -0400

    asd

--- src/app/(app)/(lobby)/stores/[storeId]/@store/page.tsx
+++ src/app/(app)/(lobby)/stores/[storeId]/@store/page.tsx
@@ -28,7 +28,7 @@ export default async function StorePage({
               alt={store.slug}
               width={90}
               height={90}
-              className="rounded-full"
+              className="rounded-2xl"
             />
             <div className="grid gap-1">
               <div className="flex flex-col justify-center items-start">
