e436e060c12dec451d298fbd89e521433930d279
Ainsdev <ainsn1654@gmail.com>
2024-06-26 22:19:15 -0400

    refactor: Remove unused database migration files and update default values in schema files

--- src/lib/db/migrations/0025_breezy_scream.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX IF EXISTS `brand_idx`;--> statement-breakpoint
-DROP INDEX IF EXISTS `size_idx`;--> statement-breakpoint
-DROP INDEX IF EXISTS `condition_idx`;--> statement-breakpoint
-DROP INDEX IF EXISTS `category_idx`;--> statement-breakpoint
-CREATE UNIQUE INDEX `brand_idx` ON `posts` (`lower("brand")`);--> statement-breakpoint
-CREATE UNIQUE INDEX `size_idx` ON `posts` (`lower("size")`);--> statement-breakpoint
-CREATE UNIQUE INDEX `condition_idx` ON `posts` (`lower("condition")`);--> statement-breakpoint
-CREATE UNIQUE INDEX `category_idx` ON `posts` (`lower("category_id")`);
