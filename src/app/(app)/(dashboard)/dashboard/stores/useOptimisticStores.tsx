2e9288d756f12599bf985e8edf6f4b0621cdee6b
Ainsdev <ainsn1654@gmail.com>
2024-07-18 12:42:53 -0400

    fix vercel prod

--- src/app/(app)/(dashboard)/dashboard/stores/useOptimisticStores.tsx
+++ src/app/(app)/(dashboard)/dashboard/stores/useOptimisticStores.tsx
@@ -1,47 +1,43 @@
-
 import { type Store, type CompleteStore } from "@/lib/db/schema/stores";
 import { OptimisticAction } from "@/lib/utils";
 import { useOptimistic } from "react";
 
 export type TAddOptimistic = (action: OptimisticAction<Store>) => void;
 
-export const useOptimisticStores = (
-  stores: CompleteStore[],
-  
-) => {
+export const useOptimisticStores = (stores: CompleteStore[]) => {
   const [optimisticStores, addOptimisticStore] = useOptimistic(
     stores,
     (
       currentState: CompleteStore[],
-      action: OptimisticAction<Store>,
+      action: OptimisticAction<Store>
     ): CompleteStore[] => {
       const { data } = action;
 
-      
-
       const optimisticStore = {
         ...data,
-        
+
         id: "optimistic",
       };
 
       switch (action.action) {
         case "create":
           return currentState.length === 0
-            ? [optimisticStore]
-            : [...currentState, optimisticStore];
+            ? [{ ...optimisticStore, id: 0 }]
+            : [...currentState, { ...optimisticStore, id: 0 }];
         case "update":
           return currentState.map((item) =>
-            item.id === data.id ? { ...item, ...optimisticStore } : item,
+            item.id === data.id
+              ? { ...item, ...optimisticStore, id: Number(item.id) }
+              : item
           );
         case "delete":
           return currentState.map((item) =>
-            item.id === data.id ? { ...item, id: "delete" } : item,
+            item.id === data.id ? { ...item, deleted: true } : item
           );
         default:
           return currentState;
       }
-    },
+    }
   );
 
   return { addOptimisticStore, optimisticStores };
